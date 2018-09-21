16ddae7e31e9b6e112b1ce09d76731b11fa817e2
madelene99 <madelene991144@gmail.com>
2018-09-21 14:56:28 +0300

    first commit

---
+++ send.php
@@ -0,0 +1,38 @@
+<?php
+//Данные для передачи
+$ip = $_SERVER['REMOTE_ADDR'];
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$code = aer5can87g; //Код потока
+$subid1=$_POST['subid1'];
+$subid2=$_POST['subid2'];
+$subid3=$_POST['subid3'];
+$subid=$subid1.":".$subid2.":".$subid3;
+ 
+$api_key = 'e9d8b87f09ed5867fed16d0987bdfe7f'; //Api ключ
+
+$params = array(
+
+'client' => $name,
+'tel' => $phone,
+'ip' => $ip,
+'code' => $code,
+'subid' => $subid,
+'traffic_type' => '0'
+
+);
+
+if($curl = curl_init()){
+
+curl_setopt($curl, CURLOPT_URL, 'http://api.monsterleads.pro/method/order.add?api_key='.$api_key);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
+$resp = curl_exec($curl); 
+curl_close($curl);
+
+}
+
+header ('Location: success.html');
+
+?>
